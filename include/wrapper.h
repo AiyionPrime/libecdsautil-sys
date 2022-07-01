90597a7b87470d99bd1caa7fa4a89b38f021d2ce
Matthias Schiffer <mschiffer@universe-factory.net>
2022-07-01 16:10:40 +0200

    add libuecc library typedefs
    a (temporary) hack until libuecc has its own rust bindings
    
    Co-authored-by: "aiyion.prime <git@aiyionpri.me>"

--- include/wrapper.h
+++ include/wrapper.h
@@ -1,4 +1,61 @@
 /*
+  Copyright (c) 2012-2015, Matthias Schiffer <mschiffer@universe-factory.net>
+  Partly based on public domain code by Matthew Dempsky and D. J. Bernstein.
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef _LIBUECC_ECC_H_
+#define _LIBUECC_ECC_H_
+
+#include <stdint.h>
+
+
+/**
+ * A 256 bit integer
+ *
+ * All functions of libuecc treat \ref ecc_int256_t as unsigned little-endian.
+ */
+typedef union _ecc_int256 {
+	/** Data bytes */
+	uint8_t p[32];
+} ecc_int256_t;
+
+/**
+ * A point on the curve unpacked for efficient calculation
+ *
+ * The internal representation of an unpacked point isn't unique, so for serialization
+ * it should always be packed.
+ */
+typedef struct _ecc_25519_work {
+	uint32_t X[32];
+	uint32_t Y[32];
+	uint32_t Z[32];
+	uint32_t T[32];
+} ecc_25519_work_t;
+
+#endif /* _LIBUECC_ECC_H_ */
+
+/*
   Copyright (c) 2012, Nils Schneider <nils@nilsschneider.net>
   Copyright (c) 2016, Matthias Schiffer <mschiffer@universe-factory.net>
   All rights reserved.
@@ -26,8 +83,6 @@
 
 #pragma once
 
-
-#include <libuecc/ecc.h>
 
 #include <stdbool.h>
 #include <stddef.h>
